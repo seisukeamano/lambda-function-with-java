36876460341797bee1fc19bed1b345dcbd4d1e97
s-amano <seisuke.arvense@gmai.com>
2021-07-14 01:47:06 +0900

    サンプルコード１を作成

---
+++ hello-maven-world/src/main/java/hello/main/HelloWorld.java
@@ -0,0 +1,18 @@
+package hello.main;
+
+/**
+ * MavenでHello World
+ */
+public class HelloWorld {
+
+	/**
+     * main処理
+     * 標準出力にHello Worldを表示する。
+     * @param args コマンド引数
+     */
+	public static void main(String[] args) {
+
+		System.out.println("Hello Maven World!!");
+	}
+
+}
