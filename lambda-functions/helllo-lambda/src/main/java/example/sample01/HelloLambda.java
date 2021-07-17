24b644111a9489db1ae73b18ff7dc31624bf5fb6
n-oouchi <noouchi4@gmail.com>
2021-07-17 11:54:13 +0900

    リクエストをMapで受け取ってgetでパラメータを取得するようにした

--- lambda-functions/helllo-lambda/src/main/java/example/sample01/HelloLambda.java
+++ lambda-functions/helllo-lambda/src/main/java/example/sample01/HelloLambda.java
@@ -1,39 +1,35 @@
-package example.sample01;
+package helloworld;
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
+import java.util.Map;
+
 /**
  * 受け取った名前情報を利用して挨拶文を返すLambda関数
- * 
- * @param <UserName> Lambda関数の実行時にインプット（リクエスト）として受け取る名前
- * @param <String>  アウトプット（レスポンス）として返答する挨拶文
+ * [RequestParam]
+ * {
+ * "firstName":"first",
+ * "lastName":"last"
+ * }
+ *
+ * @param <Map<String, Object>> Lambda関数の実行時にインプット（リクエスト）として受け取るJsonオブジェクト
+ * @param <String>     アウトプット（レスポンス）として返答する挨拶文
  */
-public class HelloLambda implements RequestHandler<UserName, String> {
+public class HelloLambda implements RequestHandler<Map<String, Object>, String> {
 
-	/**
-	 * handleRequestメソッド
-	 * Lambda関数の実行リクエストを取り扱う
-	 * @param username 名前
-	 * @param context AWS Lambda Context オブジェクト
-	 * @return 挨拶文
-	 */
-	@Override
-	public String handleRequest(UserName username, Context context) {
-
-		String greeting = String.format("Hello %s %s !", username.firstName, username.lastName);
-		return greeting;
-	}
-
-}
-
-/**
- * 名前の情報を格納するクラス
- */
-class UserName {
-
-	//名
-	public String firstName;
-	
-	//姓
-	public String lastName;
+    /**
+     * handleRequestメソッド
+     * Lambda関数の実行リクエストを取り扱う
+     *
+     * @param objectMap InputJsonオブジェクト
+     * @param context   AWS Lambda Context オブジェクト
+     * @return 挨拶文
+     */
+    @Override
+    public String handleRequest(Map<String, Object> objectMap, Context context) {
+        String firstName = objectMap.get("firstName").toString();
+        String lastName = objectMap.get("lastName").toString();
+        return String.format("Hello %s %s !", firstName, lastName);
+    }
 }
